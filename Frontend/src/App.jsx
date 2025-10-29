b573c0c9a782dce31d35edceb2ab0868a12821ed
fedorkovaleriia <fedorkovaleriia@student.uzhnu.edu.ua>
2025-10-29 10:37:02 +0200

    створила сторінку error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -6,6 +6,7 @@ import Register from './pages/Register';
 import LocationDirectory from './pages/LocationDirectory';
 import MiniGuide from './pages/MiniGuide';
 import Account from './pages/Account';
+import Error from './pages/Error';
 import './assets/fonts/fonts.css';
 import 'leaflet/dist/leaflet.css';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/locationdirectory" element={<LocationDirectory />} />
         <Route path="/miniguide" element={<MiniGuide />} />
         <Route path="/account" element={<Account />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </Router>
   );
